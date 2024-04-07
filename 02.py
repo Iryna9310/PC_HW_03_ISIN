4434bd25de98ef2dd3c75df4e50bdc3578745a1a
Iryna Sinchuk <irinashabalina100393@gmail.com>
2024-04-07 12:28:31 +0300

    modified

--- 02.py
+++ 02.py
@@ -4,7 +4,7 @@ import time
 def factorize_single(num, output_queue):
     # Функція для знаходження множників числа num
     factors = [i for i in range(1, num + 1) if num % i == 0]
-    output_queue.put(factors)
+    output_queue.put((num, factors))
 
 def factorize_parallel(*numbers):
     # Функція для паралельного розкладання чисел на множники
@@ -19,21 +19,25 @@ def factorize_parallel(*numbers):
     for process in processes:
         process.join()
 
-    results = [output_queue.get() for _ in numbers]
+    # Отримуємо результати та зберігаємо їх разом із відповідними числами
+    results = {}
+    for _ in numbers:
+        num, factors = output_queue.get()
+        results[num] = factors
     return results
 
 if __name__ == '__main__':
     # Виміряємо час виконання паралельної версії
     start_time = time.time()
-    a, b, c, d = factorize_parallel(128, 255, 99999, 10651060)
+    results = factorize_parallel(128, 255, 99999, 10651060)
     end_time = time.time()
 
     print("Parallel execution time:", end_time - start_time, "seconds")
 
     # Тест паралельної версії
-    assert a == [1, 2, 4, 8, 16, 32, 64, 128]
-    assert b == [1, 3, 5, 15, 17, 51, 85, 255]
-    assert c == [1, 3, 9, 41, 123, 271, 369, 813, 2439, 11111, 33333, 99999]
-    assert d == [1, 2, 4, 5, 7, 10, 14, 20, 28, 35, 70, 140, 76079, 152158, 304316, 380395, 532553, 760790, 1065106, 1521580, 2130212, 2662765, 5325530, 10651060]
+    assert results[128] == [1, 2, 4, 8, 16, 32, 64, 128]
+    assert results[255] == [1, 3, 5, 15, 17, 51, 85, 255]
+    assert results[99999] == [1, 3, 9, 41, 123, 271, 369, 813, 2439, 11111, 33333, 99999]
+    assert results[10651060] == [1, 2, 4, 5, 7, 10, 14, 20, 28, 35, 70, 140, 76079, 152158, 304316, 380395, 532553, 760790, 1065106, 1521580, 2130212, 2662765, 5325530, 10651060]
 
     print("All tests passed successfully.")
